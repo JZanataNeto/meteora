d89bf876e7007f3458f472d085136d6c93923344
JZanataNeto <cartasdojoao@gmail.com>
2025-07-28 16:08:21 -0300

    second commit

--- js/imprimirUmDeCadaCategoria.js
+++ js/imprimirUmDeCadaCategoria.js
@@ -5,7 +5,7 @@ function adicionarProduto(camiseta) {
     nome: camiseta.nome,
     preco: camiseta.preco,
     descricao: camiseta.descricao,
-    imagens: camiseta.imagens
+    imagens: camiseta.imagens,
   });
 }
 
@@ -39,7 +39,36 @@ export function imprimirUmDeCadaCategoria(produtos) {
         </div>
       `;
 
+      const gerarHTMLCarrossel = (imagens, nome) => {
+        if (typeof imagens !== "object" || Array.isArray(imagens)) {
+          console.error("as imagens devem ser enviadas como um objeto");
+          return "";
+        }
+        const urlsImagens = [imagens.mobile, imagens.tablet, imagens.desktop];
+        return `
+        <div id="carrossel${nome}" class="slideshow-container">
+          ${urlsImagens
+            .map(
+              (urlImagem, indice) => `
+              <div class="meusSlides-${nome} fade">
+                <img src=${urlImagem} alt="imagem ${indice}" style="width: 100%" />
+              </div>
+            `
+            )
+            .join("")}
+          <a class="anterior" id="anterior-${nome}">&#10094;</a>
+          <a class="proxima" id="proxima-${nome}">&#10095;</a>
+        </div>
+        `;
+      };
+
       card.innerHTML = images + cardBody;
+      card.className = "card card-animado";
+
+      const carrossel = gerarHTMLCarrossel(
+        produto.imagens,
+        produto.nome.replace(/\s+/g, "-")
+      );
 
       const modalContent = `
         <div class="modal-content">
@@ -51,8 +80,7 @@ export function imprimirUmDeCadaCategoria(produtos) {
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <img class="modal-imagem" src="${produto.imagens.desktop}" alt="${produto.nome
-        }">
+            ${carrossel}
             <div>
               <h3>${produto.nome}</h3>
               <p class="modal-description">${produto.descricao}</p>
@@ -88,30 +116,32 @@ export function imprimirUmDeCadaCategoria(produtos) {
               <hr class="divider-secondary">
 
               <p><b>Tamanho</b></p>
-              <form>
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="flexRadio" id="flexRadio1">
-                  <label class="form-check-label" for="flexRadio1">
-                    P
-                  </label>
-                </div>
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="flexRadio" id="flexRadio2">
-                  <label class="form-check-label" for="flexRadio2">
-                    M
-                  </label>
-                </div>
-                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="flexRadio" id="flexRadio3">
-                  <label class="form-check-label" for="flexRadio3">
-                    G
-                  </label>
-                </div>
+               <form>
+              <label class="radio-container" for="tamanho-p">
+                <input type="radio" name="tamanho-p" value="P" id="tamanho-p"/>
+                <span class="checkmark"></span>
+                <span class="radio-label">P</span>
+              </label>
+
+              <label class="radio-container" for="tamanho-m">
+                <input type="radio" name="tamanho-m" value="M" id="tamanho-m"/>
+                <span class="checkmark"></span>
+                <span class="radio-label">M</span>
+              </label>
+
+              <label class="radio-container" for="tamanho-g">
+                <input type="radio" name="tamanho-g" value="G" id="tamanho-g"/>
+                <span class="checkmark"></span>
+                <span class="radio-label">G</span>
+              </label>
               </form>
             </div>
           </div>
     <div class="modal-footer">
-            <button type="button" class="btn botao-lilas" id="adicionar-btn-${produto.nome.replace(/\s+/g, "-")}">Adicionar à sacola</button>
+            <button type="button" class="btn botao-lilas" id="adicionar-btn-${produto.nome.replace(
+              /\s+/g,
+              "-"
+            )}">Adicionar à sacola</button>
           </div>
         </div>
       `;
@@ -126,11 +156,67 @@ export function imprimirUmDeCadaCategoria(produtos) {
 
       document.body.insertAdjacentHTML("beforeend", modal);
 
-      const botao = document.querySelector(`#adicionar-btn-${produto.nome.replace(/\s+/g, "-")}`);
+      const botao = document.querySelector(
+        `#adicionar-btn-${produto.nome.replace(/\s+/g, "-")}`
+      );
       botao.addEventListener("click", () => adicionarProduto(produto));
+      let indiceSlide = 1;
+      const mostrarSlides = (numero) => {
+        const slides = document.querySelectorAll(
+          `.meusSlides-${produto.nome.replace(/\s+/g, "-")}`
+        );
+        if (numero > slides.length) indiceSlide = 1;
+        if (numero < 1) indiceSlide = slides.length;
+        slides.forEach((slide) => (slide.style.display = "none"));
+        slides[indiceSlide - 1].style.display = "block";
+      };
+
+      const maisSlides = (numero) => mostrarSlides((indice += numero));
+      document.getElementById(
+        `anterior-${produto.nome.replace(/\s+/g, "-")}`
+      ).onclick = () => {
+        maisSlides(-1);
+      };
+      document.getElementById(
+        `proxima-${produto.nome.replace(/\s+/g, "-")}`
+      ).onclick = () => {
+        maisSlides(1);
+      };
+      mostrarSlides(indiceSlide);
     }
   }
 
-  // Adicionando o container ao corpo da página
-  row.appendChild(card);
+  function elementoEstaNoViewport(elemento) {
+    const retangulo = elemento.getBoundingClientRect();
+    return (
+      retangulo.top >= 0 &&
+      retangulo.left >= 0 &&
+      retangulo.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      retangulo.right <=
+        (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function elementoEstaNaSecaoDosCards(elemento) {
+    const secaoDosCards = document.getElementById("produtos");
+    const retanguloElemento = elemento.getBoundingClientRect();
+    const retanguloSecao = secaoDosCards.getBoundingClientRect();
+    return (
+      retanguloElemento.top >= retanguloSecao.top &&
+      retanguloElemento.bottom <= retanguloSecao.bottom
+    );
+  }
+
+  function verificarVisibilidadeDosCards() {
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card) => {
+      if (elementoEstaNaSecaoDosCards(card) && elementoEstaNoViewport(card)) {
+        card.classList.add("fade-in");
+      }
+    });
+  }
+
+  verificarVisibilidadeDosCards();
+  window.addEventListener("scroll", verificarVisibilidadeDosCards);
 }
