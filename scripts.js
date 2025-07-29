71499a9b590267584f3819450447c896ca6c6ccd
JZanataNeto <cartasdojoao@gmail.com>
2025-07-29 15:35:15 -0300

    implementando parallax

--- scripts.js
+++ scripts.js
@@ -1,12 +1,8 @@
-
 import { carregarItensDaSacola } from "./js/carregarItensDaSacola.js";
 import { imprimirCamisetas } from "./js/imprimirCamisetas.js";
 import { imprimirUmDeCadaCategoria } from "./js/imprimirUmDeCadaCategoria.js";
 
-
 // Chama a função para carregar os itens da sacola ao carregar a página
-
-
 
 // Função para verificar se a página atual é /camisetas.html
 function isPaginaCamisetas() {
@@ -39,3 +35,36 @@ fetch("./json/produtos.json")
     } else imprimirUmDeCadaCategoria(produtosJSON);
   })
   .catch((error) => console.error("Erro ao carregar o arquivo JSON:", error));
+
+document.addEventListener("DOMContentLoaded", function () {
+  const containerSelecionarPersonalizado = document.querySelector(
+    ".container-selecionar-personalizado"
+  );
+  const opcaoSelecionada = document.querySelector(".opcao-selecionada");
+  const containerOpcoes =
+    containerSelecionarPersonalizado.querySelector(".container-opcoes");
+  const opcoes = containerOpcoes.querySelectorAll(".opcao");
+
+  const alternarOpcoes = () => {
+    if (containerOpcoes.classList.contains("abrir")) {
+      containerOpcoes.classList.remove("abrir");
+      containerSelecionarPersonalizado.classList.remove("abrir");
+    } else {
+      containerOpcoes.classList.add("abrir");
+      containerSelecionarPersonalizado.classList.add("abrir");
+      containerOpcoes.style.height = containerOpcoes.scrollHeight + "px";
+    }
+  };
+
+  opcaoSelecionada.addEventListener("click", alternarOpcoes);
+
+  opcoes.forEach((opcao) => {
+    opcao.addEventListener("click", (evento) => {
+      opcaoSelecionada.textContent = evento.target.textContent;
+      opcaoSelecionada.dataset.value = evento.target.dataset.value;
+      containerOpcoes.classList.remove("abrir");
+      containerSelecionarPersonalizado.classList.remove("abrir");
+      containerOpcoes.style.height = 0;
+    });
+  });
+});
